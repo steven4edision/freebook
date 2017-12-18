c9b78ebdfee58586520bc3be8e43d9355ecc4ff4
steven4edsion <17701257625@163.com>
2017-12-18 17:44:06 +0800

    切换分支，增加baseactivity

---
+++ app/src/main/java/vide/steve/com/videoproject/ui/BaseActivity.java
@@ -0,0 +1,14 @@
+package vide.steve.com.videoproject.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 类名:
+ * 作者: steven
+ * 时间: 2017/12/18 16:47
+ * 备注: (参考资料的链接等等)
+ */
+
+public class BaseActivity extends AppCompatActivity{
+
+}
