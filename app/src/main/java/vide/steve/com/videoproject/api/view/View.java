64a41ed445763d60fd104ea5281967b766c312ab
steven4edision <17701257625@163.com>
2017-12-22 01:32:57 +0800

    prepare to use retrofit and rxjava

---
+++ app/src/main/java/vide/steve/com/videoproject/api/view/View.java
@@ -0,0 +1,8 @@
+package vide.steve.com.videoproject.api.view;
+
+/**
+ * Created by admin on 2017/12/22.
+ */
+
+public class View {
+}
---
+++ app/src/main/java/vide/steve/com/videoproject/api/view/booklist/BookListView.java
@@ -0,0 +1,10 @@
+package vide.steve.com.videoproject.api.view.booklist;
+
+import vide.steve.com.videoproject.api.view.View;
+
+/**
+ * Created by admin on 2017/12/22.
+ */
+
+public class BookListView extends View{
+}
